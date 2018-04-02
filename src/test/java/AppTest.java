ff443a648da06b18151c14c98b044f3139a9c2c2
Gabrielli George <gabrielli.george@gmail.com>
2018-04-02 23:20:35 +0300

    Added tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -43,4 +43,124 @@ public class AppTest{
 
         Assert.assertEquals(size + 1, repository.getAllProducts().size());
     }
+
+    @Test
+    public void testAddMaxIntCode() throws IOException {
+        Product product = new Product();
+
+        product.setCategory(PRODUCT_CATEGORY);
+        product.setCode(Integer.MAX_VALUE + 1);
+        product.setName(PRODUCT_NAME);
+        product.setQuantity(PRODUCT_QUANTITY);
+        product.setSupplier(PRODUCT_SUPPLIER);
+
+        int size = repository.getAllProducts().size();
+
+        repository.addNewProduct(product);
+
+        Assert.assertEquals(size, repository.getAllProducts().size());
+    }
+
+    @Test
+    public void testAddMaxIntQuantity() throws IOException {
+        Product product = new Product();
+
+        product.setCategory(PRODUCT_CATEGORY);
+        product.setCode(PRODUCT_CODE);
+        product.setName(PRODUCT_NAME);
+        product.setQuantity(Integer.MAX_VALUE + 1);
+        product.setSupplier(PRODUCT_SUPPLIER);
+
+        int size = repository.getAllProducts().size();
+
+        repository.addNewProduct(product);
+
+        Assert.assertEquals(size, repository.getAllProducts().size());
+    }
+
+
+    @Test
+    public void testAddNullCategory() throws IOException {
+        Product product = new Product();
+
+        product.setCategory(null);
+        product.setCode(PRODUCT_CODE);
+        product.setName(PRODUCT_NAME);
+        product.setQuantity(Integer.MAX_VALUE + 1);
+        product.setSupplier(PRODUCT_SUPPLIER);
+
+        int size = repository.getAllProducts().size();
+
+        repository.addNewProduct(product);
+
+        Assert.assertEquals(size, repository.getAllProducts().size());
+    }
+
+    @Test
+    public void testAddNullSupplier() throws IOException {
+        Product product = new Product();
+
+        product.setCategory(PRODUCT_CATEGORY);
+        product.setCode(PRODUCT_CODE);
+        product.setName(PRODUCT_NAME);
+        product.setQuantity(Integer.MAX_VALUE + 1);
+        product.setSupplier(null);
+
+        int size = repository.getAllProducts().size();
+
+        repository.addNewProduct(product);
+
+        Assert.assertEquals(size, repository.getAllProducts().size());
+    }
+
+    @Test
+    public void testAddNullName() throws IOException {
+        Product product = new Product();
+
+        product.setCategory(PRODUCT_CATEGORY);
+        product.setCode(PRODUCT_CODE);
+        product.setName(null);
+        product.setQuantity(Integer.MAX_VALUE + 1);
+        product.setSupplier(PRODUCT_SUPPLIER);
+
+        int size = repository.getAllProducts().size();
+
+        repository.addNewProduct(product);
+
+        Assert.assertEquals(size, repository.getAllProducts().size());
+    }
+
+    @Test
+    public void testAddNullCode() throws IOException {
+        Product product = new Product();
+
+        product.setCategory(PRODUCT_CATEGORY);
+        product.setCode(0);
+        product.setName(PRODUCT_NAME);
+        product.setQuantity(Integer.MAX_VALUE + 1);
+        product.setSupplier(PRODUCT_SUPPLIER);
+
+        int size = repository.getAllProducts().size();
+
+        repository.addNewProduct(product);
+
+        Assert.assertEquals(size, repository.getAllProducts().size());
+    }
+
+    @Test
+    public void testAddNullQuantity() throws IOException {
+        Product product = new Product();
+
+        product.setCategory(PRODUCT_CATEGORY);
+        product.setCode(PRODUCT_CODE);
+        product.setName(PRODUCT_NAME);
+        product.setQuantity(-1);
+        product.setSupplier(PRODUCT_SUPPLIER);
+
+        int size = repository.getAllProducts().size();
+
+        repository.addNewProduct(product);
+
+        Assert.assertEquals(size, repository.getAllProducts().size());
+    }
 }
